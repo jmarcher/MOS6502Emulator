901fd4e2aa8dd600f98f3351e7961d96255ae7ef
Joaquín Marcher <joaquin@marcher.com.uy>
2021-02-04 11:30:55 +0100

    Add unit tests for memory reset

---
+++ tests/ResetMemoryTests.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"
+#include "CPU.h"
+#include "Memory.h"
+class ResetMemoryTests : public testing::Test
+{
+public:
+    Memory mem;
+    CPU cpu;
+
+    virtual void SetUp()
+    {
+        cpu.Reset( mem );
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(ResetMemoryTests, TestResetStatusOfCPU )
+{
+
+// when:
+cpu.Reset( mem );
+
+// Test some random addresses
+//then:
+EXPECT_EQ( mem[0x0FFF], 0x0 );
+EXPECT_EQ( mem[0x1000], 0x0 );
+EXPECT_EQ( mem[0x1001], 0x0 );
+EXPECT_EQ( mem[0x1002], 0x0 );
+//....
+EXPECT_EQ( mem[0x1009], 0x0 );
+EXPECT_EQ( mem[0x100A], 0x0 );
+EXPECT_EQ( mem[0x100B], 0x0 );
+EXPECT_EQ( mem[0x100C], 0x0 );
+EXPECT_EQ( mem[0xFFFF], 0x0 );
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,3 +1,4 @@
+#if 0
 #include "lib/Memory.h"
 #include "lib/CPU.h"
 
@@ -17,3 +18,29 @@ int main() {
     cpu.Execute(9, memory);
     return 0;
 }
+#endif
+
+#include "gtest/gtest.h"
+
+#if GTEST_OS_ESP8266 || GTEST_OS_ESP32
+#if GTEST_OS_ESP8266
+extern "C" {
+#endif
+    void setup() {
+        testing::InitGoogleTest();
+    }
+
+    void loop() { RUN_ALL_TESTS(); }
+
+#if GTEST_OS_ESP8266
+}
+#endif
+
+#else
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+#endif
