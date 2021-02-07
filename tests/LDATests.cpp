baa167017088c262960c6634ec2781fcb323a939
Joaquín Marcher <joaquin@marcher.com.uy>
2021-02-07 12:00:11 +0100

    Add tests 94% coverage

--- tests/LDATests.cpp
+++ tests/LDATests.cpp
@@ -46,3 +46,52 @@ TEST_F(LDATests, TestThatAfterLoadingAImmediatlyTheFlagsAreCorrectlySetWithDiffe
     EXPECT_EQ( cpu.N, 0x0 );
 
 }
+
+TEST_F(LDATests, TestLdaZP)
+{
+    // When
+    memory[CPU::PC_START_ADDRESS] = CPU::INS_LDA_ZP;
+    memory[CPU::PC_START_ADDRESS + 1] = 0x15;
+    memory[0x15] = 0xBB;
+    cpu.Execute(3, memory);
+
+// Test that the flags are correct
+//then:
+    EXPECT_EQ( cpu.A, 0xBB );
+    EXPECT_EQ( cpu.Z, 0x0 );
+    EXPECT_EQ( cpu.N, 0x1 );
+
+}
+
+TEST_F(LDATests, TestLdaZPX)
+{
+    // When
+    memory[CPU::PC_START_ADDRESS] = CPU::INS_LDA_ZPX;
+    memory[CPU::PC_START_ADDRESS + 1] = 0x15;
+    memory[0x15] = 0xBB;
+    cpu.Execute(4, memory);
+
+// Test that the flags are correct
+//then:
+    EXPECT_EQ( cpu.A, 0xBB );
+    EXPECT_EQ( cpu.Z, 0x0 );
+    EXPECT_EQ( cpu.N, 0x1 );
+
+}
+
+TEST_F(LDATests, TestLdaZPX_with_X)
+{
+    // When
+    cpu.X = 0x1;
+    memory[CPU::PC_START_ADDRESS] = CPU::INS_LDA_ZPX;
+    memory[CPU::PC_START_ADDRESS + 1] = 0x15;
+    memory[0x16] = 0xBB;
+    cpu.Execute(4, memory);
+
+// Test that the flags are correct
+//then:
+    EXPECT_EQ( cpu.A, 0xBB );
+    EXPECT_EQ( cpu.Z, 0x0 );
+    EXPECT_EQ( cpu.N, 0x1 );
+
+}
